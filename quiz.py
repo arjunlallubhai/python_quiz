3b96a9eb1ce3da0ff9144c638a4eb723b8e96ec1
arjunlallubhai <80367001+arjunlallubhai@users.noreply.github.com>
2022-11-10 14:55:07 +1300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -18,7 +18,7 @@ if ready == "Yes":
 else:
     print("Too bad, you're doing it anyway!")
 print("")
- 
+print("Just want to say that if you want to quit at any point, just type 'Exit' as an answer for a question in the quiz, and it will and the quiz for you.")
 #List of questions and answers
 #Easy questions and answers
 Questions_Easy = ["Who was the lead singer of the British rock band Queen? ", "Which band released the album 'The Dark side of the moon' in 1973? ", "Which rock band was fronted by Jim Morrison? (The) ", "Which of the following singers were NEVER in a mainstream band? \n A: David Byrne \n B: Phil Collins \n C: David Bowie \n D: George Michael  (Answser A, B, C, or D) ",
@@ -31,15 +31,17 @@ Answers_Medium = ["Kurt Cobain", "Bon Jovi", "Who", "B", "Queen", "Another Brick
 
 #Hard questions and answers
 Questions_Hard = ["Who was the lead singer of the Austrian rock band INXS? ", "Which band released the album 'Heartbeat city' in 1984? (The) ", "Which rock band was fronted by David Lee Roth? ", " What of the following is NOT a New Zealand band? \n A: Split Enz \n B: Cold Chisel \n C: Dragon \n D: The Exponents (Answser A, B, C, or D) ", "Which band released the hit song 'Great Southern Land'? ", "In which one of his music videos does Tom Petty dress up as the Mad Hatter from Alice in Wonderland? ", "David Byrne, Tina Weymouth, Jerry Harrison, and Chris Frantz were part of which famous American rock band? ", "For which famous movie franchise did John Barry write the main theme for? ", """ Which song contains the lyrics 'She's got a smile that it seems to me, reminds me of childhood memories'? """, "Paul David Hewson is more commonly known by what stage name? ", "Who released the album 'Lust for life' in 1977? ", "Who was the drummer for the British rock band Cream? ", "Which band released the album London Calling in 1979? (The) "]
-Answers_Hard = ["Michael Hutchence", "The Cars", "Van Halen", "B", "Icehouse", "Don't come around here no more", "Talking Heads", "James Bond", "Sweet Child O Mine", "Bono", "Iggy Pop", "Ginger Baker", "Clash"]
+Answers_Hard = ["Michael Hutchence", "Cars", "Van Halen", "B", "Icehouse", "Don't Come Around Here No More", "Talking Heads", "James Bond", "Sweet Child O Mine", "Bono", "Iggy Pop", "Ginger Baker", "Clash"]
  
 #Ask user to decide the difficulty of the quiz
 difficulty = input("Okay {}, do you want your quiz Easy, Medium, or Hard? ".format(name))
+difficulty = difficulty.title()
 while difficulty == "":
     difficulty = input("Okay {}, do you want your quiz Easy, Medium, or Hard? ".format(name))
-while difficulty != 'Easy':
+while difficulty != "Easy" and difficulty != "Medium" and difficulty != "Hard":
       difficulty = input("Please choose from easy, medium, or hard")
-difficulty = difficulty.title()
+      difficulty = difficulty.title()
+
 #If user chooses 'Easy', then give easy questions
 if difficulty == "Easy":
     i=0
@@ -47,9 +49,15 @@ if difficulty == "Easy":
     while i < 13:
         response = input(Questions_Easy[i])
         response = response.title()
+        while response == "":
+          response = input(Questions_Easy[i])
         if response == Answers_Easy[i]:
             print("Correct")
             score += 1
+        elif response in Answers_Easy[i]:
+          print("Close!!! The answer was " + Answers_Easy[i])
+        elif response == "Exit":
+          break
         else:
             print("Sorry, correct answer was " + Answers_Easy[i])
         i += 1
@@ -61,9 +69,15 @@ elif difficulty == "Medium":
     while i < 13:
         response = input(Questions_Medium[i])
         response = response.title()
+        while response == "":
+          response = input(Questions_Medium[i])
         if response == Answers_Medium[i]:
             print("Correct")
             score += 1
+        elif response in Answers_Medium[i]:
+          print("Close!!! The answer was " + Answers_Medium[i])
+        elif response == "Exit":
+          break
         else:
             print("Sorry, correct answer was " + Answers_Medium[i])
         i += 1
@@ -75,12 +89,20 @@ elif difficulty == "Hard":
     while i < 13:
         response = input(Questions_Hard[i])
         response = response.title()
+        while response == "":
+          response = input(Questions_Hard[i])
         if response == Answers_Hard[i]:
             print("Correct")
             score += 1
+        elif response in Answers_Hard[i]:
+          print("Close!!! The answer was " + Answers_Hard[i])
+        elif response == "Exit":
+          break
         else:
             print("Sorry, correct answer was " + Answers_Hard[i])
         i += 1
         print("")
 #Display final score
-print("Thank you for playing the quiz {}. Your score was {} out of 13." .format(name, score))
+print("Thank you for playing the quiz {}. Your score was {} out of {}." .format(name, score, i))
+
+
